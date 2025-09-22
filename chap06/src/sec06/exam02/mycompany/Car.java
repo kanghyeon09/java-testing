33f597457e0a1a8e185309612ae9f5d79b4d810a
kanghyeon09 <pooh055qwerty1234@gmail.com>
2025-09-22 12:26:28 +0900

    java test

---
+++ chap06/src/sec06/exam02/mycompany/Car.java
@@ -0,0 +1,10 @@
+package sec06.exam02.mycompany;
+
+public class Car {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ chap06/src/sec06/exam04/package1/B.java
@@ -0,0 +1,7 @@
+package sec06.exam04.package1;
+
+public class B {
+	A a1 = new A(true);
+	A a2 = new A(1);
+
+}
